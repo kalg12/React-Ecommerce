444b9c7236f5396cd5fedb716bca3130f2d7b70e
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-03-06 12:29:53 -0600

    Components and Styles

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
     mode: 'development',
     resolve:{
         extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+        }
     },
     module: {
         rules:[
@@ -21,6 +28,10 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                type: 'asset'
             },
             {
                 test: /\.html$/,
